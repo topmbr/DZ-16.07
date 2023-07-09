ca5b279ec072f6c4d494828999b1f09837a7b5ce
topmbr <centerm9911@gmail.com>
2023-07-09 18:47:55 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp28/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Xml.Serialization;
+
+Person person = new Person("Tom", 37);
+
+// Выполнение сериализации
+string xml = SerializeToXml(person);
+Console.WriteLine("Сериализованный объект:");
+Console.WriteLine(xml);
+
+// Выполнение десериализации
+Person deserializedPerson = DeserializeFromXml<Person>(xml);
+Console.WriteLine("\nДесериализованный объект:");
+Console.WriteLine($"Имя: {deserializedPerson.Name}");
+Console.WriteLine($"Возраст: {deserializedPerson.Age}");
+
+static string SerializeToXml<T>(T obj)
+{
+    XmlSerializer serializer = new XmlSerializer(typeof(Person));
+
+    using (StringWriter writer = new StringWriter())
+    {
+        serializer.Serialize(writer, obj);
+        return writer.ToString();
+    }
+}
+
+static T DeserializeFromXml<T>(string xml)
+{
+    XmlSerializer serializer = new XmlSerializer(typeof(Person));
+
+    using (StringReader reader = new StringReader(xml))
+    {
+        return (T)serializer.Deserialize(reader);
+    }
+}
+
+//[Serializable]
+public class Person
+{
+    public string Name { get; set; } = "Undefined";
+    public int Age { get; set; } = 1;
+
+    public Person() { }
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
